beacfb52baf07bf1923f67d347f4d8d6f7922f61
Attila Süli <suli.zakar.attila@gmail.com>
2017-07-05 22:14:36 +0200

    Listeners on post insert and post update events.

---
+++ src/main/java/hu/suliatis/HibernateRegisterListener.java
@@ -0,0 +1,32 @@
+package hu.suliatis;
+
+import org.hibernate.event.service.spi.EventListenerRegistry;
+import org.hibernate.event.spi.EventType;
+import org.hibernate.internal.SessionFactoryImpl;
+import org.hibernate.jpa.HibernateEntityManagerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
+
+@Component
+public class HibernateRegisterListener {
+    @PersistenceUnit
+    private EntityManagerFactory entityManagerFactory;
+
+    @Autowired
+    private AuditEventListener auditEventListener;
+
+    @PostConstruct
+    public void registerListeners() {
+        if(entityManagerFactory instanceof HibernateEntityManagerFactory) {
+            final HibernateEntityManagerFactory  hibernateEntityManagerFactory = (HibernateEntityManagerFactory) entityManagerFactory;
+            final SessionFactoryImpl sessionFactory =  (SessionFactoryImpl) hibernateEntityManagerFactory.getSessionFactory();
+            final EventListenerRegistry registry = sessionFactory.getServiceRegistry().getService(EventListenerRegistry.class);
+            registry.getEventListenerGroup(EventType.POST_INSERT).appendListener(auditEventListener);
+            registry.getEventListenerGroup(EventType.POST_UPDATE).appendListener(auditEventListener);
+        }
+    }
+}
